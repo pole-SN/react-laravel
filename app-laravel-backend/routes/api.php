dfb5feda1c539b42697508720a3e2ca0d9102399
nara syunsuke <syunsuke.nara@ikeuchi.co.jp>
2022-06-10 10:27:25 +0900

    APIアクセスルートの設定

--- app-laravel-backend/routes/api.php
+++ app-laravel-backend/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['middleware' => 'api'])->group(function () {
+    Route::get('/', function (Request $request) {
+        return view('welcome');
+    });
+});
